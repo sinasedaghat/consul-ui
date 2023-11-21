859725d35fc5c45ec5e480f9c429aff05c59f100
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2023-11-21 11:07:39 +0330

    add colors object in tailwind config in extend object and start create basic navbar and sidebar in default layoout

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <p>Some default layout content shared across all pages</p>
-    <slot />
+  <div class="flex">
+    <div class="basis-full h-14 bg-secondary"></div>
+  </div>
+  <div class="flex h-screen">
+    <!-- side bar -->
+    <div class="basis-2/12 h-full bg-primary sm:hidden">
+    </div>
+    <!-- content -->
+    <div class="basis-8/12 p-14">
+      <slot />
+    </div>
+
   </div>
 </template>
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,21 +2,21 @@
 export default {
   content: [],
   theme: {
-    colors: {
-      // 'background': '#FFFFFF',
-      // 'surface': '#FFFFFF',
-      'primary': '#9F32B2',
-      // 'primary-darken-1': '#3700B3',
-      'secondary': '#00D7FF',
-      // 'secondary-darken-1': '#018786',
-      'alternative': '#313896',
-
-      'error': '#F04124',
-      'info': '#39A0ED',
-      'success': '#6FB72F',
-      'warning': '#FFF000',
-    },
     extend: {
+      colors: {
+        // 'background': '#FFFFFF',
+        // 'surface': '#FFFFFF',
+        'primary': '#9F32B2',
+        // 'primary-darken-1': '#3700B3',
+        'secondary': '#00D7FF',
+        // 'secondary-darken-1': '#018786',
+        'alternative': '#313896',
+  
+        'error': '#F04124',
+        'info': '#39A0ED',
+        'success': '#6FB72F',
+        'warning': '#FFF000',
+      },
       fontFamily: {
         'nunito': ['nunito', 'sans-serif']
       },
